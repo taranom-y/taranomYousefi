b936f33d8c15117ed134000e1650966f8aa14bc6
taranom yousefi <tarannom.yousefi98@gmail.com>
2022-05-11 22:27:22 +0430

    init

---
+++ src/src/Controller/AttractionController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Attraction;
+use App\Repository\AttractionRepository;
+use ContainerMS5AMTQ\EntityManager_9a5be93;
+use Doctrine\ORM\EntityManager;
+use http\Client\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AttractionController extends AbstractController
+{
+    #[Route('/attraction', name: 'app_attraction')]
+    public function index( AttractionRepository $attractionRepository,EntityManager $entityManager): Response
+    {
+        $attractions=$attractionRepository->findAll();
+        return $this->render('attraction/contactUs.html.twig', [
+            'attractions' => $attractions
+        ]);
+    }
+
+    #[Route('/attraction/new', name: 'app_attraction_new')]
+    public function new( Request $request,AttractionRepository $attractionRepository): Response
+    {
+        if ($request->isMethode() == "POST") {
+            $name = $request->get("name");
+            $shortDescription = $request->get("short_description");
+            $fullDescription = $request->get("full_description");
+            $score = $request->get("score");
+
+            $attraction = new Attraction();
+            $attraction->setName($name);
+            $attraction->setShortDescription($shortDescription);
+            $attraction->setFullDescription($fullDescription);
+            $attraction->setScore($score);
+            $attraction->setCreatedAt(new \DateTimeImmutable());
+            $attraction->setUpdatedAt(new \DateTimeImmutable());
+
+            $attractionRepository->add($attraction);
+        }
+            return $this->render('attraction/new.html.twig'
+
+            );
+        }
+
+    #[Route('/attraction/edit/{id}', name: 'app_attraction_new')]
+    public function edit( Request $request,AttractionRepository $attractionRepository,Attraction $attraction,EntityManager $entityManager): Response
+    {
+        if ($request->isMethode("POST")) {
+            $name = $request->get("name");
+            $shortDescription = $request->get("short_description");
+            $fullDescription = $request->get("full_description");
+            $score = $request->get("score");
+
+
+            $attraction->setName($name);
+            $attraction->setShortDescription($shortDescription);
+            $attraction->setFullDescription($fullDescription);
+            $attraction->setScore($score);
+            $attraction->setUpdatedAt(new \DateTimeImmutable());
+
+            $entityManager->flush();
+        }
+        return $this->render('attraction/edit.html.twig');
+    }
+
+
+    #[Route('/attraction/{id}', name: 'app_attraction_view')]
+    public function view( Attraction $attraction): Response
+    {
+
+
+        return $this->render('attraction/view.html.twig',['attraction'=>$attraction]
+
+        );
+    }
+
+    #[Route('/attraction/delete/{id}', name: 'app_attraction_delete')]
+    public function delete( Attraction $attraction,AttractionRepository $attractionRepository): Response
+    {
+
+      $attractionRepository->remove($attraction);
+        return $this->redirectToRoute('app_attraction');
+    }
+
+
+}
---
+++ src/src/Entity/Room.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoomRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoomRepository::class)]
+class Room
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $number;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $empty;
+
+    #[ORM\Column(type: 'integer')]
+    private $numberOfBeds;
+
+    #[ORM\ManyToOne(targetEntity: Hotel::class, inversedBy: 'rooms')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $hotel;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getEmpty(): ?string
+    {
+        return $this->empty;
+    }
+
+    public function setEmpty(string $empty): self
+    {
+        $this->empty = $empty;
+
+        return $this;
+    }
+
+    public function getNumberOfBeds(): ?int
+    {
+        return $this->numberOfBeds;
+    }
+
+    public function setNumberOfBeds(int $numberOfBeds): self
+    {
+        $this->numberOfBeds = $numberOfBeds;
+
+        return $this;
+    }
+
+    public function getHotel(): ?Hotel
+    {
+        return $this->hotel;
+    }
+
+    public function setHotel(?Hotel $hotel): self
+    {
+        $this->hotel = $hotel;
+
+        return $this;
+    }
+}
