b4c640580951c11bc6f2346c5d1edb0fc936c5a1
taranom yousefi <tarannom.yousefi98@gmail.com>
2022-07-01 18:14:01 +0430

    init Ass5.1

--- src/src/Model/TimeLoggerInterface.php
+++ src/src/Model/TimeLoggerInterface.php
@@ -5,7 +5,8 @@ use DateTimeImmutable;
 
 interface TimeLoggerInterface
 {
-  public function setCreatedAt(DateTimeImmutable $createdAt);
-
-  public  function  setUpdatedAt(DateTimeImmutable $updatedAt);
+    public function getCreatedAt(): \DateTimeImmutable;
+  public function setCreatedAt(DateTimeImmutable $createdAt):self;
+  public function getUpdatedAt(): \DateTimeImmutable;
+  public  function  setUpdatedAt(DateTimeImmutable $updatedAt):self;
 }
