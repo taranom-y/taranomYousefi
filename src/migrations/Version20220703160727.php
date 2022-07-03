624a864eda93a4de445d5754326aaa5189c33ad8
taranom yousefi <tarannom.yousefi98@gmail.com>
2022-07-03 20:38:49 +0430

    init complete Ass5

---
+++ src/migrations/Version20220703160727.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220703160727 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE messages ADD CONSTRAINT FK_DB021E96E104C1D3 FOREIGN KEY (created_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE messages ADD CONSTRAINT FK_DB021E96BB649746 FOREIGN KEY (updated_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519BE104C1D3');
+        $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519BBB649746');
+        $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519B3243BB18');
+        $this->addSql('DROP INDEX fk_729f519b3243bb18 ON room');
+        $this->addSql('CREATE INDEX IDX_729F519B3243BB18 ON room (hotel_id)');
+        $this->addSql('DROP INDEX fk_729f519be104c1d3 ON room');
+        $this->addSql('CREATE INDEX IDX_729F519BE104C1D3 ON room (created_user_id)');
+        $this->addSql('DROP INDEX fk_729f519bbb649746 ON room');
+        $this->addSql('CREATE INDEX IDX_729F519BBB649746 ON room (updated_user_id)');
+        $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519BE104C1D3 FOREIGN KEY (created_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519BBB649746 FOREIGN KEY (updated_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519B3243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id)');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649E104C1D3');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649BB649746');
+        $this->addSql('DROP INDEX IDX_8D93D649BB649746 ON user');
+        $this->addSql('DROP INDEX IDX_8D93D649E104C1D3 ON user');
+        $this->addSql('ALTER TABLE user DROP created_user_id, DROP updated_user_id');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE messages DROP FOREIGN KEY FK_DB021E96E104C1D3');
+        $this->addSql('ALTER TABLE messages DROP FOREIGN KEY FK_DB021E96BB649746');
+        $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519B3243BB18');
+        $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519BE104C1D3');
+        $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519BBB649746');
+        $this->addSql('DROP INDEX idx_729f519bbb649746 ON room');
+        $this->addSql('CREATE INDEX FK_729F519BBB649746 ON room (updated_user_id)');
+        $this->addSql('DROP INDEX idx_729f519b3243bb18 ON room');
+        $this->addSql('CREATE INDEX FK_729F519B3243BB18 ON room (hotel_id)');
+        $this->addSql('DROP INDEX idx_729f519be104c1d3 ON room');
+        $this->addSql('CREATE INDEX FK_729F519BE104C1D3 ON room (created_user_id)');
+        $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519B3243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id)');
+        $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519BE104C1D3 FOREIGN KEY (created_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519BBB649746 FOREIGN KEY (updated_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user ADD created_user_id INT DEFAULT NULL, ADD updated_user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649E104C1D3 FOREIGN KEY (created_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649BB649746 FOREIGN KEY (updated_user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D649BB649746 ON user (updated_user_id)');
+        $this->addSql('CREATE INDEX IDX_8D93D649E104C1D3 ON user (created_user_id)');
+    }
+}
